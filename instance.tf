bd8f2480614e99ebef85254750661a1280e79a61
DESKTOP-AL5H48L\leandro <arielchanampa12@gmail.com>
2021-11-22 13:39:56 -0300

    Ultimas modificaciones en los archivos .tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "server1" {
   ami                         = var.ami_id
   instance_type               = var.instance_type 
-  subnet_id                   = aws_subnet.subnet1.id
+  subnet_id                   = aws_subnet.subnet1_G9.id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id] #se cambio el security group
   private_ip                  = "172.16.9.0" #se cambio la ip
